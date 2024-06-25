d4bd8ab6221e9eab3633a1f29afa507cc488dbab
Cactus137 <anhdo13072004@gmail.com>
2024-06-25 13:15:57 +0700

    Lesson 1

--- Lesson 1/oop/index.php
+++ Lesson 1/oop/index.php
@@ -6,3 +6,13 @@ require "ClassA.php";
 
 $person = new ClassA("John", "Blue", 25);
 $person->show();
+
+// Age accessible because it is public
+// $person->age = 30;
+// $person->show();
+
+// Name not accessible because it is private
+// $person->name = "Jane";
+// $person->show();
+
+
